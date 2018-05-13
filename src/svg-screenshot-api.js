9a190d7139e9227eba4d2b8dade87d27560437fd
daiiz <da.iz.mg@gmail.com>
2018-05-13 17:09:55 +0900

    svg, metadataは揃った

---
+++ src/svg-screenshot-api.js
@@ -0,0 +1,10 @@
+export function uploadToSvgScreenshot ({ svg, url, image, title, viewbox, dpr }) {
+  console.log(
+    svg.substring(0, 30),
+    url,
+    image.substring(0, 30),
+    title,
+    viewbox,
+    dpr
+  )
+}
