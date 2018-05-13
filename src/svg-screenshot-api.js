ba787acfc0132826f416bb4a0bf62bc249393e83
daiiz <da.iz.mg@gmail.com>
2018-05-13 17:37:10 +0900

    local serverに向けてuploadできた

--- src/svg-screenshot-api.js
+++ src/svg-screenshot-api.js
@@ -1,10 +1,40 @@
-export function uploadToSvgScreenshot ({ svg, url, image, title, viewbox, dpr }) {
-  console.log(
-    svg.substring(0, 30),
-    url,
-    image.substring(0, 30),
+import fs from 'fs'
+import axios from 'axios'
+
+const accessTokenFilename = "./svg-screenshot-token.json"
+const appUrl = 'http://localhost:8080'
+
+const existFile = filePath => {
+  try {
+    fs.statSync(filePath)
+    return true
+  } catch(err) {
+    return false
+  }
+}
+
+const apiClient = axios.create({
+  timeout: 60000
+})
+
+export async function uploadToSvgScreenshot ({ svg, url, image, title, viewbox, dpr }) {
+  if (!existFile(accessTokenFilename)) return
+  const { access_token } = JSON.parse(fs.readFileSync(accessTokenFilename))
+  const uploadApiEndpoint = `${appUrl}/_ah/api/items/v1/upload?access_token=${access_token}`
+
+  const item = {
+    svg,
+    orgurl: url,
+    base64png: image,
     title,
     viewbox,
     dpr
-  )
+  }
+
+  try {
+    const { data } = await apiClient.post(uploadApiEndpoint, item)
+    console.log(data)
+  } catch (err) {
+    console.error(err.message)
+  }
 }
